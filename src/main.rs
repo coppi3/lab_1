cfe1acbbc6d147eef730266cf82f73bac237256d
coppi3 <coppiemertz@gmail.com>
2022-10-18 21:01:27 +0300

    feat: 🎸 finished first task
    Added a complete output for the first task

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ use std::fmt;
 use std::fs;
 
 fn main() {
-    const init_msg: &str = "Цыдыпов А.О.";
-    let encoded_msg = encode(init_msg);
-    println!("{:X}", V(&encoded_msg));
+    const INITMSG: &str = "Цыдыпов А.О.";
+    let encoded_msg = encode(INITMSG);
+    println!("Исходное сообщение: {}", INITMSG);
+    println!("В шетснадцатеричном коде: {:X}", V(&encoded_msg));
+    println!("В двоичном коде: {:08b}", V(&encoded_msg));
+    println!(
+        "Длина сообщения: {} байт ({} бит)",
+        encoded_msg.len(),
+        encoded_msg.len() * 8
+    );
 }
